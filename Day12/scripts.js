fe3668ae3c79a9aca02f35d0ea2dbfc89a215f01
DaDDy <blacklistuser420@gmail.com>
2024-01-27 22:39:29 +0630

    Day 12 assignment finish

--- Day12/scripts.js
+++ Day12/scripts.js
@@ -1,51 +1,68 @@
 var result;
-var operator1 = +
+
 function start() {
-    var startbtn = document.getElementById("start");
-    var stopbtn = document.getElementById("stop");
-    var question = document.getElementById("random");
-  var num1 = Math.floor(Math.random() * 100);
-  var num2 = Math.floor(Math.random() * 100);
-  result = num1 + num2;
-  question.innerText = num1 + " + " + num2;
-  startbtn.classList.remove("activeStart");
-  startbtn.classList.add("inactiveStart");
+  var startbtn = document.getElementById("startbtn");
+  var stopbtn = document.getElementById("stopbtn");
+  var answer = document.getElementById("answer");
+
+  startbtn.setAttribute("class", "btnstart inactiveStart");
   startbtn.disabled = true;
+  stopbtn.setAttribute("class", "btnstop activeStop");
   stopbtn.disabled = false;
-  stopbtn.classList.remove("inactiveStop");
-  stopbtn.classList.add("activeStop");
+  answer.disabled = false;
+
+  quiz();
 }
 
 function stop() {
-    var startbtn = document.getElementById("start");
-    var stopbtn = document.getElementById("stop");
-    var history = document.getElementById("history");
-    var question = document.getElementById("random");
+  var startbtn = document.getElementById("startbtn");
+  var stopbtn = document.getElementById("stopbtn");
+  var history = document.getElementById("history");
+  var random = document.getElementById("random");
+  var answer = document.getElementById("answer");
 
-    stopbtn.classList.add("inactiveStop");
-    stopbtn.classList.remove("activeStop");
-    startbtn.classList.add("activeStart");
-    startbtn.classList.remove("inactiveStart");
-    stopbtn.disabled = true;
+  startbtn.setAttribute("class", "btnstart activeStart");
+  stopbtn.setAttribute("class", "btnstop inactiveStop");
   startbtn.disabled = false;
-    question.innerText='';
-    history.innerHTML='';
+  stopbtn.disabled = true;
+  answer.disabled = true;
+
+  answer.value = "";
+  random.innerText = "";
+  history.innerHTML = "";
 }
 
 function check() {
-    var answer = document.getElementById("answer");
-var history = document.getElementById("history");
-var question = document.getElementById("random");
-var num1 = Math.floor(Math.random() * 100);
-var num2 = Math.floor(Math.random() * 100);
-    if(result == answer.value){
-        history.innerHTML+= `<li>correct</li>`
-    }else{
-        history.innerHTML+= `<li>wrong</li>`
+  var answer = document.getElementById("answer").value;
+  var history = document.getElementById("history");
 
-    }
-  
+  if (result == answer) {
+    history.innerHTML += `<li>correct</li>`;
+  } else {
+    history.innerHTML += `<li>wrong</li>`;
+  }
+
+  quiz();
+  answer = "";
+}
+
+function quiz() {
+  var random = document.getElementById("random");
+  var num1 = Math.floor(Math.random() * 100);
+  var num2 = Math.floor(Math.random() * 100);
+  var no = Math.round(Math.random() * 3);
+
+  if (no == 0) {
     result = num1 + num2;
-    question.innerText = num1 + " + " + num2;
-    answer.value=''
+    random.innerText = num1 + " + " + num2;
+  } else if (no == 1) {
+    result = num1 - num2;
+    random.innerText = num1 + " - " + num2;
+  } else if (no == 2) {
+    result = num1 * num2;
+    random.innerText = num1 + " x " + num2;
+  } else {
+    result = num1 / num2;
+    random.innerText = num1 + " ÷ " + num2;
+  }
 }
