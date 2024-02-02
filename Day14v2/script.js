61fb536633d2526aba298668a3b3fe2292b61d40
DaDDy <blacklistuser420@gmail.com>
2024-02-02 22:39:09 +0630

    left game win and game over status

--- Day14v2/script.js
+++ Day14v2/script.js
@@ -1,3 +1,6 @@
+var clickSong = document.getElementById('clickSong')
+var gameoverSong = document.getElementById('gameoverSong')
+var gamewinSong = document.getElementById('gamewinSong')
 var boom, p1, p2, p3, p4, p5, p6, p7, p8;
 var isOver = false;
 var isWin = false;
@@ -6,11 +9,10 @@ var clicked = 0;
 
 gameStart();
 function usrPress(cell) {
-    if(isOver) return;
+    if(isOver || isWin) return;
   var id = cell.id;
   if (id == boom) {
     var boomImg = document.getElementById("boomImg");
-    cell.style.background = "linear-gradient(#FF1414,#A60505)";
     boomImg.setAttribute("class", "boom show");
     gameOver();
   } else if(cell.getAttribute('click') == 0) {
@@ -24,12 +26,17 @@ function usrPress(cell) {
       id == p7 ||
       id == p8
     ) {
-      cell.style.background = "linear-gradient(#FFF509,#D8CF09)";
+      cell.style.background = "#f4ff2b";
+
     } else {
       cell.style.background = "linear-gradient(#32FB2E,#08B204)";
+    cell.style.background = "#2bff41";
+
     }
+    clickSong.play()
     clicked++;
     cell.setAttribute('click',1);
+    gameWin()
   }else return;
 }
 
@@ -39,7 +46,7 @@ function gameStart() {
   boom = row + "" + col;
   var boomCell = document.getElementById(`${boom}`);
   var image = document.createElement("img");
-  image.setAttribute("src", "assets/boom.svg");
+  image.setAttribute("src", "assets/img/boom.svg");
   image.setAttribute("class", "boom hidden");
   image.setAttribute("id", "boomImg");
   boomCell.append(image);
@@ -54,11 +61,14 @@ function gameStart() {
 }
 
 function gameOver () {
-    for (let i = 0; i < 36; i++) document.getElementsByClassName('cell')[i].style.background="linear-gradient(#FF1414,#A60505)";
+    for (let i = 0; i < 36; i++) document.getElementsByClassName('cell')[i].style.background="#ff1c1c";
     isOver=true
-    
+    gameoverSong.play()
 }
 
 function gameWin() {
-    if(clicked)
+    if(clicked == 35){
+      gamewinSong.play();
+      isWin=true;
+    }else return;
 }
