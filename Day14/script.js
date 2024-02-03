4c493b0f403d1fd9f60cbd78b7dec02d735f7994
DaDDy <blacklistuser420@gmail.com>
2024-02-03 19:29:24 +0630

    update file

--- Day14/script.js
+++ Day14/script.js
@@ -1,103 +1,111 @@
-var mine;
-var p1;
-var p2;
-var p3;
-var p4;
-var p5;
-var p6;
-var p7;
-var p8;
+var clickSong = document.getElementById("clickSong");
+var gameoverSong = document.getElementById("gameoverSong");
+var gamewinSong = document.getElementById("gamewinSong");
+var gameStatus = document.getElementById("gameStatus");
+var statusTitle = document.getElementById("statusTitle");
+var startGame = document.getElementById("gameStart");
+var boom, p1, p2, p3, p4, p5, p6, p7, p8;
 var isOver = false;
 var isWin = false;
-var count = 0;
+var boomImage;
+var clicked = 0;
 
-mineBuild();
-function mineBuild() {
+//Game Start Button
+function start() {
+  startGame.setAttribute("class", "gamestart gamestatus_hidden");
+  gameStart();
+}
+
+//Game Start
+function gameStart() {
+  minePosition();
+  var boomCell = document.getElementById(`${boom}`);
+  boomImage = document.getElementById("boomImg");
+  if (boomImage) boomImage.setAttribute("class", "boom hidden");
+  else {
+    boomImage = document.createElement("img");
+    boomImage.setAttribute("src", "assets/img/boom.svg");
+    boomImage.setAttribute("class", "boom hidden");
+    boomImage.setAttribute("id", "boomImg");
+  }
+  boomCell.append(boomImage);
+}
+
+//Create Mine Position
+function minePosition() {
   var row = Math.floor(Math.random() * 6 + 1);
   var col = Math.floor(Math.random() * 6 + 1);
-  mine = row + "" + col;
-
-  p1 = (row - 1) + "" + (col - 1);
-  p2 = (row - 1) + "" + col;
-  p3 = (row - 1) + "" + (col + 1);
+  p1 = row - 1 + "" + (col - 1);
+  p2 = row - 1 + "" + col;
+  p3 = row - 1 + "" + (col + 1);
   p4 = row + "" + (col - 1);
   p5 = row + "" + (col + 1);
-  p6 = (row + 1) + "" + (col - 1);
-  p7 = (row + 1) + "" + col;
-  p8 = (row + 1) + "" + (col + 1);
+  p6 = row + 1 + "" + (col - 1);
+  p7 = row + 1 + "" + col;
+  p8 = row + 1 + "" + (col + 1);
+  boom = row + "" + col;
 }
 
-
-function userPress(cell) {
-    if(isOver || isWin) return;
-    var pressCell = cell.id;
-    if(pressCell == mine){
-       cell.style.backgroundColor="red";
-       cell.innerText="BOOM";
-       gameOverDisplay();
-    }else if(cell.getAttribute('click') == 0){
-        if(  
-            pressCell == p1 ||
-            pressCell == p2 ||
-            pressCell == p3 ||
-            pressCell == p4 ||
-            pressCell == p5 ||
-            pressCell == p6 ||
-            pressCell == p7 ||
-            pressCell == p8
-        ){
-            cell.style.backgroundColor="yellow";
-        }else{
-            cell.style.backgroundColor="green";
-    
-        }
-        cell.setAttribute('click',true)
-        count++;
+//Check Safe cells or Mine Cell
+function usrPress(cell) {
+  if (isOver || isWin) return;
+  var id = cell.id;
+  if (id == boom) gameOver();
+  else if (cell.getAttribute("click") == 0) {
+    if (
+      id == p1 ||
+      id == p2 ||
+      id == p3 ||
+      id == p4 ||
+      id == p5 ||
+      id == p6 ||
+      id == p7 ||
+      id == p8
+    ) {
+      cell.style.background = "#f4ff2b";
+    } else {
+      cell.style.background = "linear-gradient(#32FB2E,#08B204)";
+      cell.style.background = "#2bff41";
     }
-    gameWinDisplay();
-  
+    clickSong.play();
+    clicked++;
+    cell.setAttribute("click", 1);
+    gameWin();
+  } else return;
 }
 
-function playClick() {
-    //play click sound
+//Game Over
+function gameOver() {
+  isOver = true;
+  boomImage = document.getElementById("boomImg");
+  boomImage.setAttribute("class", "boom show");
+  for (let i = 0; i < 36; i++)
+    document.getElementsByClassName("cell")[i].style.background = "#ff1c1c";
+  gameoverSong.play();
+  gameStatus.setAttribute("class", "gamestatus gamestatus_show");
+  statusTitle.innerText = "game over";
 }
 
-
-function gamOverSound() {
-    //play game over sound
+//Game Win
+function gameWin() {
+  if (clicked == 35) {
+    isWin = true;
+    gamewinSong.play();
+    gameStatus.setAttribute("class", "gamestatus gamestatus_show");
+    statusTitle.innerText = "game win";
+  } else return;
 }
 
-function gameOverDisplay () {
-    isOver=true
-    for (let i = 0; i < 36; i++) {
-       document.getElementsByClassName('cell')[i].style.backgroundColor='red'
-        
-    }
-
-    document.getElementById('32').innerText='G'
-    document.getElementById('33').innerText='A'
-    document.getElementById('34').innerText='M'
-    document.getElementById('35').innerText='E'
-    document.getElementById('42').innerText='O'
-    document.getElementById('43').innerText='V'
-    document.getElementById('44').innerText='E'
-    document.getElementById('45').innerText='R'
-}
-
-function gameWinDisplay () {
-    if(count == 35){
-        isWin = true;
-        for (let i = 0; i < 36; i++) {
-            document.getElementsByClassName('cell')[i].style.backgroundColor='green'
-             
-         }
-     
-         document.getElementById('32').innerText='G'
-         document.getElementById('33').innerText='A'
-         document.getElementById('34').innerText='M'
-         document.getElementById('35').innerText='E'
-         document.getElementById('43').innerText='w'
-         document.getElementById('44').innerText='I'
-         document.getElementById('45').innerText='N'
-    }
+//Game Retry
+function gameRetry() {
+  isOver = false;
+  isWin = false;
+  clicked = 0;
+  for (let i = 0; i < 36; i++) {
+    var cell = document.getElementsByClassName("cell")[i];
+    cell.style.background = "white";
+    cell.setAttribute("click", 0);
+  }
+  gameStatus.setAttribute("class", "gamestatus gamestatus_hidden");
+  gameStart();
 }
