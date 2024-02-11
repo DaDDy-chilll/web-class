1e05aa74f29af5c3783e39800516b34df0031c6a
DaDDy <blacklistuser420@gmail.com>
2024-02-12 00:02:24 +0630

    update

--- Day16/script.js
+++ Day16/script.js
@@ -1,16 +1,26 @@
 let fishs = document.querySelectorAll("#fish");
 const bubble = document.getElementById("bubble");
+const windowHeight = window.innerHeight - 85;
+(() => {
+  fishPosition();
+})();
 
-startAni();
-function startAni() {
-  console.log("hi");
+function fishPosition() {
 
-  fishs.forEach((fish,index) => {
-   
-    const randomNo = Math.floor(Math.random() * 600) + index;
-    const position  = (randomNo > 600 || randomNo < 100) ? randomNo : randomNo
-    console.log(position);
-    // if (position > 600 || position < 100) return;
+  fishs.forEach((fish, index) => {
+    const randomNo = Math.floor(Math.random() * windowHeight) + index;
+    console.log(randomNo);
+    const randomNo1 = Math.floor(Math.random() * 7) + 1;
+    // if (randomNo1 % 2 === 0)
+      fish.style.transform = `translate(-300px,0) scaleX(-1)`;
+    
+    
+    // else  fish.style.transform = `translate(1500px,0)`;
+    const position = randomNo > 600 || randomNo < 100 ? 400 : randomNo;
     fish.style.top = `${position}px`;
   });
+}
+
+function bubblePosition() {
+
 }
