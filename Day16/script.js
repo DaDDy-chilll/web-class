c6272353a39a8c3d828633dab10341711388b72e
DaDDy <blacklistuser420@gmail.com>
2024-02-16 10:57:14 +0630

    update

--- Day16/script.js
+++ Day16/script.js
@@ -6,21 +6,33 @@ const windowHeight = window.innerHeight - 85;
 })();
 
 function fishPosition() {
-
   fishs.forEach((fish, index) => {
     const randomNo = Math.floor(Math.random() * windowHeight) + index;
-    console.log(randomNo);
     const randomNo1 = Math.floor(Math.random() * 7) + 1;
-    // if (randomNo1 % 2 === 0)
-      fish.style.transform = `translate(-300px,0) scaleX(-1)`;
-    
-    
-    // else  fish.style.transform = `translate(1500px,0)`;
+
+    leftOrRight(fish,index)
     const position = randomNo > 600 || randomNo < 100 ? 400 : randomNo;
     fish.style.top = `${position}px`;
   });
 }
 
-function bubblePosition() {
+function leftOrRight (fish,index) {
+  const aniRandom = Math.floor(Math.random() * 3) + 1;
+  const aniRandom1 = Math.floor(Math.random() * 4) + 1;
+  if (index < 4) {
+    fish.style.transformOrigin='right';
+    fish.style.transform = `translate(-300px,0) scaleX(-1)`
+    fish.style.animation = `fishani${index} 10s linear ${aniRandom}s infinite`
+  }
+  else {
+    fish.style.transformOrigin='left';
+    fish.style.transform = `translate(1500px,0)`;
+    fish.style.animation = `fishani${index} 10s linear  ${aniRandom1}s infinite`
+  }
+}
 
+function bubblePosition() {
+  const bubble = document.createElement('img')
+  bubble.src = 'assets/bubble.png'
+  
 }
