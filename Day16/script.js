593d8dc1565b7c05bc2474e58a2104357498fc93
DaDDy <blacklistuser420@gmail.com>
2024-02-16 21:34:35 +0630

    all assignment finish

--- Day16/script.js
+++ Day16/script.js
@@ -1,47 +1,54 @@
 let fishs = document.querySelectorAll("#fish");
 const bubble = document.querySelectorAll("#bubble");
-const bubbleContainer = document.getElementById("bubble_conatiner")
+const bubbleContainer = document.getElementById("bubble_conatiner");
 const windowHeight = window.innerHeight - 85;
 const windowWidth = window.innerWidth;
+
+//Start Fish Func and Bubble Func
 (() => {
   fishPosition();
-  bubblePosition()
+  generateBubble();
 })();
 
+// Generate Height of fish positon
 function fishPosition() {
   fishs.forEach((fish, index) => {
     const randomNo = Math.floor(Math.random() * windowHeight) + index;
-    leftOrRight(fish,index)
+    leftOrRight(fish, index);
     const position = randomNo > 600 || randomNo < 100 ? 400 : randomNo;
     fish.style.top = `${position}px`;
   });
 }
 
-function leftOrRight (fish,index) {
+/**
+ * @param {Element of fish} fish
+ * @param {Index of fish} index
+ * @description index is less than 4 to the right and greather than 4 to left
+ */
+function leftOrRight(fish, index) {
   const aniRandom = Math.floor(Math.random() * 3) + 1;
   const aniRandom1 = Math.floor(Math.random() * 4) + 1;
   if (index < 4) {
-    fish.style.transformOrigin='right';
-    fish.style.transform = `translate(-400px,0) scaleX(-1)`
-    fish.style.animation = `fishani${index} 10s linear ${aniRandom}s infinite`
-  }
-  else {
-    fish.style.transformOrigin='left';
+    fish.style.transformOrigin = "right";
+    fish.style.transform = `translate(-400px,0) scaleX(-1)`;
+    fish.style.animation = `fishani${index} 10s linear ${aniRandom}s infinite`;
+  } else {
+    fish.style.transformOrigin = "left";
     fish.style.transform = `translate(1500px,0)`;
-    fish.style.animation = `fishani${index} 10s linear  ${aniRandom1}s infinite`
+    fish.style.animation = `fishani${index} 10s linear  ${aniRandom1}s infinite`;
   }
 }
 
-function bubblePosition() {
- for (let i = 0; i < 30; i++) {
-  const withRandom = Math.floor(Math.random() * 5) + 1;
-  const delayRandom = Math.floor(Math.random() * 30) + 1;
-  console.log('width', withRandom);
-  const bubble = document.createElement('img')
-  bubble.src='assets/bubble.png'
-  bubble.style.width=`${withRandom}%`
-  bubble.style.animation=`bubbleAni  ${i}s linear ${delayRandom}s  infinite`
-  bubbleContainer.append(bubble)
-  
- }
+//Create element of image and loop of this element
+function generateBubble() {
+  for (let i = 0; i < 30; i++) {
+    const withRandom = Math.floor(Math.random() * 5) + 1;
+    const delayRandom = Math.floor(Math.random() * 30) + 1;
+    console.log("width", withRandom);
+    const bubble = document.createElement("img");
+    bubble.src = "assets/bubble.png";
+    bubble.style.width = `${withRandom}%`;
+    bubble.style.animation = `bubbleAni  ${i}s linear ${delayRandom}s  infinite`;
+    bubbleContainer.append(bubble);
+  }
 }
