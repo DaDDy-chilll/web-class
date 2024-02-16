8c31a5fdb2dcdfe052b8a8ee586e83e604cb2240
DaDDy <blacklistuser420@gmail.com>
2024-02-16 18:12:28 +0630

    finish assignment

--- Day16/script.js
+++ Day16/script.js
@@ -1,15 +1,16 @@
 let fishs = document.querySelectorAll("#fish");
-const bubble = document.getElementById("bubble");
+const bubble = document.querySelectorAll("#bubble");
+const bubbleContainer = document.getElementById("bubble_conatiner")
 const windowHeight = window.innerHeight - 85;
+const windowWidth = window.innerWidth;
 (() => {
   fishPosition();
+  bubblePosition()
 })();
 
 function fishPosition() {
   fishs.forEach((fish, index) => {
     const randomNo = Math.floor(Math.random() * windowHeight) + index;
-    const randomNo1 = Math.floor(Math.random() * 7) + 1;
-
     leftOrRight(fish,index)
     const position = randomNo > 600 || randomNo < 100 ? 400 : randomNo;
     fish.style.top = `${position}px`;
@@ -21,7 +22,7 @@ function leftOrRight (fish,index) {
   const aniRandom1 = Math.floor(Math.random() * 4) + 1;
   if (index < 4) {
     fish.style.transformOrigin='right';
-    fish.style.transform = `translate(-300px,0) scaleX(-1)`
+    fish.style.transform = `translate(-400px,0) scaleX(-1)`
     fish.style.animation = `fishani${index} 10s linear ${aniRandom}s infinite`
   }
   else {
@@ -32,7 +33,15 @@ function leftOrRight (fish,index) {
 }
 
 function bubblePosition() {
+ for (let i = 0; i < 30; i++) {
+  const withRandom = Math.floor(Math.random() * 5) + 1;
+  const delayRandom = Math.floor(Math.random() * 30) + 1;
+  console.log('width', withRandom);
   const bubble = document.createElement('img')
-  bubble.src = 'assets/bubble.png'
+  bubble.src='assets/bubble.png'
+  bubble.style.width=`${withRandom}%`
+  bubble.style.animation=`bubbleAni  ${i}s linear ${delayRandom}s  infinite`
+  bubbleContainer.append(bubble)
   
+ }
 }
