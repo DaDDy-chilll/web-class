886c08d1225083ec3999e35f8805ed9561d6d398
DaDDy <blacklistuser420@gmail.com>
2024-03-03 22:09:50 +0630

    day22 assignment finish

--- Day22/script.js
+++ Day22/script.js
@@ -6,13 +6,16 @@ var backspace = document.getElementById("backspace");
 var tabs = document.querySelectorAll("#tab");
 var enters = document.querySelectorAll("#enter");
 var space = document.getElementById("space");
-var input = document.getElementById('text')
+var input = document.getElementById("text");
+var alertStatus = document.getElementById("alertStatus");
+var alertText = document.getElementById("alertText");
+var closeBtn = document.getElementById("close");
+
 methods.forEach((method) => {
   method.addEventListener("click", () => {
     var value = input.value;
     var text = display.innerText;
-    stringMethodsFunc(method,text,value)
- 
+    stringMethodsFunc(method, text, value);
   });
 });
 
@@ -22,61 +25,97 @@ esc.addEventListener("click", () => escKeyFunc());
 backspace.addEventListener("click", () => backSpaceFunc());
 enters.forEach((enter) => enter.addEventListener("click", () => enterFunc()));
 space.addEventListener("click", () => sapcefunc());
-var keyFunc = (key) => (display.innerText += key.innerText);
+closeBtn.addEventListener("click", () => {
+  alertStatus.style.transform = "translateY(-100px)";
+});
 
-var escKeyFunc = () => (display.innerText = "");
+var keyFunc = (key) => (display.innerText += key.innerText);
+var escKeyFunc = () => {display.innerText = "";input.value=''};
 var backSpaceFunc = () => (display.innerText = display.innerText.slice(0, -1));
 var tabFunc = () => (display.innerText += "\t");
 var enterFunc = () => (display.innerText += "\n");
 var sapcefunc = () => (display.innerText += " ");
 
-var stringMethodsFunc = (method,text,value) => {
-    switch (method.innerText) {
-        case "charAt()":
-            checkValue(value)
-          console.log(text.charAt(+value));
-          break;
-        case "charCodeAt()":
-            checkValue(value)
-          console.log(text.charCodeAt(+value));
-          break;
-        case "toUpperCase()":
-          console.log(text.toUpperCase());
-          break;
-        case "toLowerCase()":
-          console.log(text.toLowerCase());
-          break;
-        case "trim()":
-          console.log(text.trim());
-          break;
-        case "includes()":
-            checkValue(value)
-          console.log(text.includes(value));
-          break;
-        case "indexOf()":
-            checkValue(value)
-          console.log(text.indexOf(value));
-          break;
-        case "lastIndexof()":
-            checkValue(value)
-          console.log(text.lastIndexOf(value));
-          break;
-        case "startsWith()":
-            checkValue(value)
-          console.log(text.startsWith(value));
-          break;
-        case "endsWith()":
-            checkValue(value)
-          console.log(text.endsWith(value));
-          break;
-        default:
-          break;
+var stringMethodsFunc = (method, text, value) => {
+  switch (method.innerText) {
+    case "charAt()":
+      if (value.length === 0) {
+        showWarning()
+        return;
+      } else {
+        showAlert(text.charAt(value));
       }
-}
+      break;
+    case "charCodeAt()":
+      if (value.length === 0) {
+        showWarning()
+        return;
+      } else {
+        showAlert(text.charCodeAt(value));
+      }
+      break;
+    case "toUpperCase()":
+      
+    showAlert(text.toUpperCase());
+      break;
+    case "toLowerCase()":
+      showAlert(text.toLowerCase());
+      break;
+    case "trim()":
+      showAlert(text.trim());
+      break;
+    case "includes()":
+      if (value.length === 0) {
+        showWarning()
+        return;
+      } else {
+        showAlert(text.includes(value));
+      }
+      break;
+    case "indexOf()":
+      if (value.length === 0) {
+        showWarning()
+        return;
+      } else {
+        var ans = text.indexOf(value);
 
-var checkValue = (value) => {
-    if(value.length === 0){
-        input.classList.add('show_Warning')
-    }
-    setTimeout(()=>input.classList.remove('show_Warning'),1500)
+        showAlert(ans === -1 ? "false":ans);
+      }
+      break;
+    case "lastIndexof()":
+      if (value.length === 0) {
+        showWarning()
+        return;
+      } else {
+        showAlert(text.lastIndexOf(value));
+      }
+      break;
+    case "startsWith()":
+      if (value.length === 0) {
+        showWarning()
+        return;
+      } else {
+        showAlert(text.startsWith(value));
+      }
+      break;
+    case "endsWith()":
+      if (value.length === 0) {
+        showWarning()
+        return;
+      } else {
+        showAlert(text.endsWith(value));
+      }
+      break;
+    default:
+      break;
+  }
+};
+
+var showWarning = () => {
+  input.classList.add("show_Warning");
+  setTimeout(() => input.classList.remove("show_Warning"), 1500);
 }
+var showAlert = (result) => {
+  alertText.innerText = `the result is = ${result}`;
+  alertStatus.style.transform = "translate(0)";
+};
