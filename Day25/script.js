80eb099c6f91fc0e5d6addda5ac793c6fc9ec08f
DaDDy <blacklistuser420@gmail.com>
2024-03-08 19:57:17 +0630

    Day25 assignment start

--- Day25/script.js
+++ Day25/script.js
@@ -1,22 +1,29 @@
-const container = document.getElementById('container');
-
-
-
-(() => {
+const container = document.getElementById("container");
+let initialValue = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+]
+  /**
+   * @description Set up game / Create Row and cell with loop;
+   */
+  setupGame();
+  const setupGame = () => {
     const conatinerWidth = Math.floor(container.offsetWidth / 90);
     const containerHeight = Math.floor(container.offsetHeight / 90);
-    console.log(containerHeight,conatinerWidth);
-    for (let i = 0; i < containerHeight; i++) {
-    const row = document.createElement('div')
-    row.className = 'row'
+    for (let i = 0; i < containerHeight - 1; i++) {
+      const row = document.createElement("div");
+      row.className = "row";
+      row.id = `row${i + 1}`;
+      for (let j = 0; j < conatinerWidth - 1; j++) {
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cell.id = `${i + 1},${j + 1}`;
+        row.appendChild(cell.cloneNode());
+      }
+      container.appendChild(row);
+    }
+  }
 
-        for (let j = 0; j < conatinerWidth; j++) {
-    const cell = document.createElement('div')
-    cell.className = 'cell';
-
-            row.appendChild(cell.cloneNode())
-        }
-        container.appendChild(row.cloneNode())
-
-   }
-})()
