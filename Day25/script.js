c217ad7faa55089693f73f27d8b665231f066c86
DaDDy <blacklistuser420@gmail.com>
2024-03-09 19:45:24 +0630

    update

--- Day25/script.js
+++ Day25/script.js
@@ -5,25 +5,50 @@ let initialValue = [
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
-]
-  /**
-   * @description Set up game / Create Row and cell with loop;
-   */
-  setupGame();
-  const setupGame = () => {
-    const conatinerWidth = Math.floor(container.offsetWidth / 90);
-    const containerHeight = Math.floor(container.offsetHeight / 90);
-    for (let i = 0; i < containerHeight - 1; i++) {
-      const row = document.createElement("div");
-      row.className = "row";
-      row.id = `row${i + 1}`;
-      for (let j = 0; j < conatinerWidth - 1; j++) {
-        const cell = document.createElement("div");
-        cell.className = "cell";
-        cell.id = `${i + 1},${j + 1}`;
-        row.appendChild(cell.cloneNode());
-      }
-      container.appendChild(row);
-    }
-  }
+];
+const player1 = 1;
+const player2 = 2;
+let Finished = false;
+let currentPlayer = player1;
+/**
+ * @description Set up game / Create Row and cell with loop;
+ */
 
+
+
+
+
+(() => {
+  const conatinerWidth = Math.floor(container.offsetWidth / 90);
+  const containerHeight = Math.floor(container.offsetHeight / 90);
+  for (let i = 0; i < containerHeight - 1; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    row.id = `row${i + 1}`;
+    for (let j = 0; j < conatinerWidth - 1; j++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.setAttribute('data-cell',`${i + 1},${j + 1}`)
+      // cell.onclick = userClick(`${i + 1},${j + 1}`)
+      cell.id='cell'
+      row.appendChild(cell.cloneNode());
+    }
+    container.appendChild(row);
+  }
+  allCells()
+  
+})();
+
+// setupGame();
+
+
+function userClick (id){
+  console.log(id);
+}
+
+function allCells () {
+const cells = document.querySelectorAll('#cell');
+cells.forEach(cell => {
+  cell.addEventListener('click',userClick(cell.getAttribute('data-cell')))
+})
+}
