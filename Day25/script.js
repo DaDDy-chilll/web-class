6e32d68a78d0fab91427276c04e35a10fff8d4f5
Who <blacklistuser420@gmail.com>
2024-03-10 09:53:45 +0000

    update

--- Day25/script.js
+++ Day25/script.js
@@ -1,4 +1,7 @@
 const container = document.getElementById("container");
+const conatinerWidth = Math.floor(container.offsetWidth / 90);
+const containerHeight = Math.floor(container.offsetHeight / 90);
+let cells=[];
 let initialValue = [
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
@@ -10,34 +13,49 @@ const player1 = 1;
 const player2 = 2;
 let Finished = false;
 let currentPlayer = player1;
-/**
- * @description Set up game / Create Row and cell with loop;
- */
-
-
-
-
 
 (() => {
-  const conatinerWidth = Math.floor(container.offsetWidth / 90);
-  const containerHeight = Math.floor(container.offsetHeight / 90);
-  for (let i = 0; i < containerHeight - 1; i++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    row.id = `row${i + 1}`;
-    for (let j = 0; j < conatinerWidth - 1; j++) {
-      const cell = document.createElement("div");
-      cell.className = "cell";
-      cell.setAttribute('data-cell',`${i + 1},${j + 1}`)
-      // cell.onclick = userClick(`${i + 1},${j + 1}`)
-      cell.id='cell'
-      row.appendChild(cell.cloneNode());
-    }
-    container.appendChild(row);
-  }
-  allCells()
+  initialValue.forEach((row,j) => {
+    const rowElement = document.createElement('div')
+    rowElement.className='row';
+    row.forEach((cell,i) => {
+      const cellElement = document.createElement('div')
+      cellElement.className='cell';
+      cellElement.id=`${j},${i}`;
+      // cellElement.onclick="userClick(i)"
+      rowElement.appendChild(cellElement);
+
+    })
+    container.appendChild(rowElement);
+
+  })
+  cells = document.getElementsByClassName('cell')
+})()
+
+
+fun
+
+
+// (() => {
+
+//   for (let i = 0; i < containerHeight - 1; i++) {
+//     const row = document.createElement("div");
+//     row.className = "row";
+//     row.id = `row${i + 1}`;
+//     for (let j = 0; j < conatinerWidth - 1; j++) {
+//       const cell = `<div class='cell' id="${i + 1},${j + 1}" onclick='${userClick(this)}'></div>`
+//       const cell = document.createElement("div");
+//       cell.className = "cell";
+//       cell.setAttribute('data-cell',`${i + 1},${j + 1}`)
+//       cell.onclick = userClick(`${i + 1},${j + 1}`)
+//       cell.id='cell'
+//       row.append(cell);
+//     }
+//     container.appendChild(row);
+//   }
+//   allCells()
   
-})();
+// })();
 
 // setupGame();
 
@@ -46,9 +64,9 @@ function userClick (id){
   console.log(id);
 }
 
-function allCells () {
-const cells = document.querySelectorAll('#cell');
-cells.forEach(cell => {
-  cell.addEventListener('click',userClick(cell.getAttribute('data-cell')))
-})
-}
+// function allCells () {
+// const cells = document.querySelectorAll('#cell');
+// cells.forEach(cell => {
+//   cell.addEventListener('click',userClick(cell.getAttribute('data-cell')))
+// })
+// }
