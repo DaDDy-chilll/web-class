e6ece25e1d7a3a40d321a361ec06402835b1523f
Who <blacklistuser420@gmail.com>
2024-03-17 09:20:52 +0000

    update

--- Day25/main.js
+++ Day25/main.js
@@ -2,6 +2,10 @@ import { menuToggle } from "./utils/index.js"
 
 const lines = document.querySelectorAll('#menuLine')
 const navMenu = document.getElementById('navMenu')
+const currDateInput = document.getElementById('currInput')
+const currBtn = document.getElementById('currBtn')
 
 
 navMenu.addEventListener('click', () => menuToggle(lines))
+
+currBtn.addEventListener("click", () => currDateInput.showPicker())
