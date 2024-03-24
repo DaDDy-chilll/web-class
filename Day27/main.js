25d466d695dab6159200fc5bb811930ac42bd896
DaDDy <blacklistuser420@gmail.com>
2024-03-24 19:54:14 +0630

    update

--- Day27/main.js
+++ Day27/main.js
@@ -56,12 +56,35 @@ let setupFinish = false;
   const newPlanets = planets.sort(() => Math.random() - 0.5);
   newPlanets.forEach((planet) => {
     let card = `
-         <div class="card" data-planet=${planet.name}>
-         <img src="assets/universe/${planet.name}.jpg" alt="${planet.name}" width="100%">
+  
+
+        <div
+          class="card"
+          data-planet=${planet.name}
+          data-tilt
+          data-tilt-speed="400"
+          data-tilt-max="20"
+          data-tilt-scale="1.2"
+          data-tilt-reverse="true"
+          id='card'
+        >
+          <div class="card_front"></div>
+
+          <div class="card_back">
+          <img src="assets/universe/${planet.name}.jpg" alt="${planet.name}" width="100%">
          <img src="assets/planets/${planet.name}.png" alt="${planet.name}" class="planets" width="50%">
          <p class="planets_name">${planet.name}</p>
-         </div>
+          </div>
+        </div>
     `;
     cardContainer.innerHTML += card;
   });
+  VanillaTilt.init(document.querySelectorAll(".card"));
+
 })();
+
+function userClick(element) {
+  console.log(element);
+}
+
+
